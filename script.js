ece39a06b6fcb3919d4d30e96c7dd2586010dd7b
Sanjay Sri Thanu Malaiyaan <sanjaychittu2005@gmail.com>
2025-12-22 19:47:24 +0530

    your message

---
+++ script.js
@@ -0,0 +1,94 @@
+// // Variables
+// var a=100;
+//  let b=200;
+//  const c=300;
+//  {
+//     var a=100;
+//  let b=200;
+//  const c=300;
+//  }
+// var a=200;
+//  b=300;
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+// Data types
+
+let a=10;
+let b=20;
+console.log(a);
+console.log(b);
+console.log(typeof(a));
+console.log(typeof(b));
+
+// 2 string
+let cname="BIT";
+let grade='A';
+console.log(cname);
+console.log(typeof((cname)));
+console.log(typeof((grade)));
+// booolean 3
+let pass=true;
+console.log(pass)
+console.log(typeof(pass));
+// null 4
+let data=null;
+console.log(typeof(data));
+//  undefined 5
+let  val;
+console.log(typeof(val));
+// bigint
+let phoneno=223456789;
+console.log(typeof(phoneno));
+
+let age=25;
+console.log("MY name is ", cname ,"My age is",age);
+//  operators
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(2**3);
+
+//  assignment operators
+
+console.log(a+=b);
+console.log(a);
+
+// comparison operator
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+
+// logical 
+console.log(a >b && a==b);
+
+console.log('10' * 10);
+
+
+let marks=Number(prompt("Enter your mark"));
+if(marks>=90 && marks<=100){
+    console.log("A grade");
+}
+else if(marks>=60 && marks<=89){
+    console.log("B grade");
+}
+else{
+    console.log
+}
+
+
+
+
+
+
+
+
