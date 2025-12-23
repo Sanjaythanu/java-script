4f146dfad0dd9febce85887e95b770cce554fe41
Sanjay Sri Thanu Malaiyaan <sanjaychittu2005@gmail.com>
2025-12-23 21:00:15 +0530

    sanjay

--- script.js
+++ script.js
@@ -1,227 +1,182 @@
-// // // // // // Variables
-// // // // // var a=100;
-// // // // //  let b=200;
-// // // // //  const c=300;
-// // // // //  {
-// // // // //     var a=100;
-// // // // //  let b=200;
-// // // // //  const c=300;
-// // // // //  }
-// // // // // var a=200;
-// // // // //  b=300;
-// // // // //  console.log(a);
-// // // // //  console.log(b);
-// // // // //  console.log(c);
+// Variables
+var a = 100;
+let b = 200;
+const c = 300;
 
-// // // // // Data types
+{
+    var a = 100;
+    let b = 200;
+    const c = 300;
+}
 
-// // // // let a=10;
-// // // // let b=20;
-// // // // console.log(a);
-// // // // console.log(b);
-// // // // console.log(typeof(a));
-// // // // console.log(typeof(b));
+var a = 200;
+b = 300;
+console.log(a);
+console.log(b);
+console.log(c);
 
-// // // // // 2 string
-// // // // let cname="BIT";
-// // // // let grade='A';
-// // // // console.log(cname);
-// // // // console.log(typeof((cname)));
-// // // // console.log(typeof((grade)));
-// // // // // booolean 3
-// // // // let pass=true;
-// // // // console.log(pass)
-// // // // console.log(typeof(pass));
-// // // // // null 4
-// // // // let data=null;
-// // // // console.log(typeof(data));
-// // // // //  undefined 5
-// // // // let  val;
-// // // // console.log(typeof(val));
-// // // // // bigint
-// // // // let phoneno=223456789;
-// // // // console.log(typeof(phoneno));
+// Data types
+let x = 10;
+let y = 20;
+console.log(x);
+console.log(y);
+console.log(typeof x);
+console.log(typeof y);
 
-// // // // let age=25;
-// // // // console.log("MY name is ", cname ,"My age is",age);
-// // // // //  operators
+// String
+let cname = "BIT";
+let grade = 'A';
+console.log(cname);
+console.log(typeof cname);
+console.log(typeof grade);
 
-// // // // console.log(a+b);
-// // // // console.log(a-b);
-// // // // console.log(a*b);
-// // // // console.log(a/b);
-// // // // console.log(a%b);
-// // // // console.log(2**3);
+// Boolean
+let pass = true;
+console.log(pass);
+console.log(typeof pass);
 
-// // // // //  assignment operators
+// Null
+let data = null;
+console.log(typeof data);
 
-// // // // console.log(a+=b);
-// // // // console.log(a);
+// Undefined
+let val;
+console.log(typeof val);
 
-// // // // // comparison operator
+// Number
+let phoneno = 223456789;
+console.log(typeof phoneno);
 
-// // // // console.log(a>b);
-// // // // console.log(a<b);
-// // // // console.log(a>=b);
-// // // // console.log(a<=b);
-// // // // console.log(a==b);
-// // // // console.log(a!=b);
-// // // // console.log(a===b);
+let age = 25;
+console.log("My name is", cname, "My age is", age);
 
-// // // // // logical 
-// // // // console.log(a >b && a==b);
+// Operators
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(2 ** 3);
 
-// // // // console.log('10' * 10);
+// Assignment operator
+console.log(x += y);
+console.log(x);
 
+// Comparison operators
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+console.log(x === y);
 
-// // // // let marks=Number(prompt("Enter your mark"));
-// // // // if(marks>=90 && marks<=100){
-// // // //     console.log("A grade");
-// // // // }
-// // // // else if(marks>=60 && marks<=89){
-// // // //     console.log("B grade");
-// // // // }
-// // // // else{
-// // // //     console.log
-// // // // }
+// Logical operator
+console.log(x > y && x == y);
 
+// Type coercion
+console.log('10' * 10);
 
-// // // // function fact(n){
-// // // //     if(n<=1)
-// // // //         return n;
-// // // //     return n*fact(n-1);
-// // // // }
-// // // // fact(5)
+// Conditional statement
+let marks = Number(prompt("Enter your mark"));
+if (marks >= 90 && marks <= 100) {
+    console.log("A grade");
+} else if (marks >= 60 && marks <= 89) {
+    console.log("B grade");
+} else {
+    console.log("Fail");
+}
 
+// Recursion
+function fact(n) {
+    if (n <= 1)
+        return 1;
+    return n * fact(n - 1);
+}
+console.log(fact(5));
 
-// // // // const add=(a,b) =>{
-// // // //     return a*b;
-// // // // }
-// // // // console.log(add(10,20));
+// Arrow function
+const add = (a, b) => {
+    return a * b;
+}
+console.log(add(10, 20));
 
+// Callback functions
+function greet(name, callback) {
+    console.log("Hello", name);
+    callback("sri");
+    typo("vijay");
+}
 
-// // // //  call back function
+function hai(name1) {
+    console.log("Batman", name1);
+}
 
+function typo(name2) {
+    console.log("tvk", name2);
+}
 
-// // // // 
+greet("Sanjay", hai);
 
-// // // // function greet(name,callback){
-// // // //     console.log("hello ",name);
-// // // //     callback("sri");
-// // // //     typo("vijay");
-// // // // }
-// // // // function hai(name1){
-// // // //     console.log("Batman ",name1);
-// // // // }
-// // // // function typo(name2){
-// // // //     console.log("tvk ",name2);
-// // // // }
-// // // // greet("Sanjay",hai);
+// setTimeout
+setTimeout(() => {
+    console.log("Vizhi veekura");
+}, 10000);
 
+// setInterval
+let count = 1;
+let timer = setInterval(() => {
+    console.log("Welcome to BIT");
+    count++;
+    if (count == 11) {
+        clearInterval(timer);
+    }
+}, 2000);
 
+// Arrays
+let array = [1, 2, 3, "BIT", true, 'a', 'b', 'c'];
+console.log(array[3]);
+array[3] = "sanjay loves sri";
+console.log(array);
+console.log(array[array.length - 1]);
 
-// // // // setTimeout(()=>{
-// // // //     console.log("Vizhi veekura")
-// // // // },10000)
+array.push('d');
+console.log(array.pop());
+array.shift();
+console.log(array);
+array.unshift("hello");
+console.log(array);
 
-
-// // // // let count=1;
-// // // // let timer =setInterval(()=>{
-// // // //     console.log("Welcome to Bit");
-// // // //     count++;
-// // // //     if(count==11){
-// // // //         clearInterval(timer);
-// // // //     }
-// // // // },2000)
-
-
-
-// // // //  Arrays
-
-// // // let array =[1,2,3,"BIT",true,'a','b','c']
-// // // console.log(array[3]);
-// // // array[3]="sanjay loves sri"
-// // // console.log(array);
-// // // console.log(array[array.length-1]);
-// // // array.push('d');
-// // // console.log(array.pop()); // ro remove last element
-// // // array.shift(); // remove first element and make the other elements in te suffix place
-// // // console.log(array);
-// // // array.unshift("hello"); // add elements in the first
-// // // console.log(array);
-//  let arr1=[5,3,2,1]
-console.log(arr1.sort()); console.log(arr1.reverse());
- console.log(arr1.includes(5));
- console.log(arr1.indexOf(2));
+// Array methods
+let arr1 = [5, 3, 2, 1];
+console.log(arr1.sort());
+console.log(arr1.reverse());
+console.log(arr1.includes(5));
+console.log(arr1.indexOf(2));
 
 console.log(arr1);
-arr1.slice(1,3);
-console.log(arr1.slice(0,3));
- arr2=[10,20,30];
-console.log(arr2); arr3=arr1.concat(arr2);
- console.log(arr3);
+console.log(arr1.slice(0, 3));
 
+let arr2 = [10, 20, 30];
+console.log(arr2);
 
-// newarray=[...arr1]//spread of in js to copy
-// arr4=[...arr2];
-// console.log(newarray);
-// console.log(arr4);
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
 
+// Spread operator
+let newarray = [...arr1];
+let arr4 = [...arr2];
+console.log(newarray);
+console.log(arr4);
 
-// heiger order functions
+// Higher-order functions
+let arr = [1, 2, 3, 4, 5];
 
-// let arr=[1,2,3,4,5];
-// let nummul=arr.map(val=>val*2);
-// console.log(nummul);
+let nummul = arr.map(val => val * 2);
+console.log(nummul);
 
+let oddsum = arr.filter(val => val % 2 != 0);
+console.log(oddsum);
 
-// let oddsum=arr.filter(val=>val%2!=0);
-// console.log(oddsum);
-
-// let numbers=arr.reduce((arr,val)=>arr*val,1);
-// console.log(numbers);
-
-//  Objects
-
-User={
-    username:"Sanjay",
-    Age:20,
-    city:"Chennai"
-
-    Login(){
-       console.log("Your are login ",User.); 
-    }
-    User2:{
-        username:"mango",
-
-    }
-
-}
-console.log(User);
-console.log(User.username);
-console.log(User.age);
-console.log(User.city);
-User.city="banglore";
-console.log(User);
-
-
-Document object model
-
-
-function changeTitle(){
-    let h1=document.getElementById('h1')
-    h1.innerHTML="Pambu ulagam";
-}
-function trickortreat(){
-    let h2=document.getElementById('h2')
-    h2.innerHTML="hello";
-}
-
-
-
-
-
-
-
-
-
+let numbers = arr.reduce((acc, val) => acc * val, 1);
+console.log(numbers);
